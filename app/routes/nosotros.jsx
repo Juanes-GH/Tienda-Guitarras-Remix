90666fa812f6f59f819e2bb2b086fe1430941b7f
JuanesGH <jheras@2amigos.us>
2023-12-14 20:23:10 +0100

    Nosotros page and tienda page done

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,7 +1,48 @@
+import imagen from "../../public/img/nosotros.jpg"
+import styles from "../styles/nosotros.css"
+
+export function meta(){
+  return (
+    [
+      {
+        title: 'GuitarLA - Sobre Nosotros',
+        descripcion: 'Venta de guitarras, blog de música'
+      }
+    ]
+  )
+}
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as:'image'
+    }
+  ]
+}
 
 function Nosotros() {
   return (
-    <div>Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+        
+      <div className="contenido">
+        <img src={imagen} alt="imagen sobre nosotros" />
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum tristique odio, vel sagittis sapien gravida nec. Ut varius sagittis egestas. Morbi at auctor mauris, sed porttitor tortor. Aliquam volutpat vel sapien vitae lobortis. Duis maximus tincidunt elementum. Donec consectetur tincidunt massa, eu finibus dui sodales sed. Nullam non neque at ligula volutpat maximus et sit amet magna. Nulla faucibus velit nec ipsum vulputate eleifend ut a nunc.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum tristique odio, vel sagittis sapien gravida nec. Ut varius sagittis egestas. Morbi at auctor mauris, sed porttitor tortor. Aliquam volutpat vel sapien vitae lobortis. Duis maximus tincidunt elementum. Donec consectetur tincidunt massa, eu finibus dui sodales sed. Nullam non neque at ligula volutpat maximus et sit amet magna. Nulla faucibus velit nec ipsum vulputate eleifend ut a nunc.
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,7 +1,54 @@
+import { useLoaderData } from "@remix-run/react"
+import {getGuitarras} from "../models/guitarras.server"
+import Guitarra from "../components/guitarra"
+import styles from "../styles/guitarras.css"
+
+export function meta(){
+  return (
+    [
+      {
+        title:'GuitarLA - Tienda de Guitarras',
+        description: 'GuitarLA - Nuestra coleccion de guitarras'
+      }
+    ]
+  )
+}
+
+export function links(){
+  return [
+    {
+      rel:'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async function loader(){
+  // -- Otra manera de consumir la datos de la API usando 'FETCH'
+  // const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
+  // const resultado = await respuesta.json()
+
+  const guitarras = await getGuitarras()
+  return guitarras.data
+}
 
 function Tienda() {
+  const guitarras = useLoaderData()
+
   return (
-    <div>Tienda</div>
+    <main className="contendor">
+      <h2 className="heading">Nuestra Colección</h2>
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map(guitarra => (
+            <Guitarra 
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      )}
+    </main>
   )
 }
 
