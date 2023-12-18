ebc27b5dc929ddc6c019df235090595ab72c2a92
JuanesGH <jheras@2amigos.us>
2023-12-18 21:53:38 +0100

    pagina blog hecha

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -1,7 +1,6 @@
 export async function getGuitarras(){
     const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
    return await respuesta.json()
-  
 }
 
 export async function getGuitarra(url){
